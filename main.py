26f1c3a358c9e070c72394e2b7ff27183519d0ec
deep3576 <deep3576@gmail.com>
2023-08-18 16:06:27 -0400

    first Commit

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+# OpenCV program to detect face in real time
+# import libraries of python OpenCV
+# where its functionality resides
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# load the required trained XML classifiers
+# https://github.com/Itseez/opencv/blob/master/
+# data/haarcascades/haarcascade_frontalface_default.xml
+# Trained XML classifiers describes some features of some
+# object we want to detect a cascade function is trained
+# from a lot of positive(faces) and negative(non-faces)
+# images.
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# https://github.com/Itseez/opencv/blob/master
+# /data/haarcascades/haarcascade_eye.xml
+# Trained XML file for detecting eyes
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# capture frames from a camera
+cap = cv2.VideoCapture(0)
+
+# loop runs if capturing has been initialized.
+while 1:
+
+	# reads frames from a camera
+	ret, img = cap.read()
+
+	# convert to gray scale of each frames
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	# Detects faces of different sizes in the input image
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	for (x,y,w,h) in faces:
+		# To draw a rectangle in a face
+		cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = img[y:y+h, x:x+w]
+
+		# Detects eyes of different sizes in the input image
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+
+		#To draw a rectangle in eyes
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
+
+	# Display an image in a window
+	cv2.imshow('img',img)
+
+	# Wait for Esc key to stop
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+
+# Close the window
+cap.release()
+
+# De-allocate any associated memory usage
+cv2.destroyAllWindows()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
