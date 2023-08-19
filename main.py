3b9204c19c1b21896545e7ace4834e22c8f9187d
InderdeepSingh <deep3576@gmail.com>
2023-08-19 08:51:48 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 cap = cv.VideoCapture(0)
+img_counter = 0
+face_classifier = cv.CascadeClassifier(
+    cv.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 if not cap.isOpened():
  print("Cannot open camera")
  exit()
@@ -10,16 +15,28 @@ while True:
  # if frame is read correctly ret is True
  if not ret:
     print("Can't receive frame (stream end?). Exiting ...")
- # Our operations on the frame come here
- # Display the resulting frame
+ face = face_classifier.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+ if len(face)==1:
+    img_name = f'opencv_frame{img_counter}.png'
+    img_counter=img_counter+1
+        # saves the image as a png file
+    cv.imwrite(img_name,frame)
+    print('screenshot taken')
+    break
+ for (x, y, w, h) in face:
+    cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
- cv.rectangle(frame,(0,0),(255,255),(233,0,255),5)
+
+
+ #cv.rectangle(face)
+
 
  cv.imshow('frame', frame)
-
  if cv.waitKey(1) == ord('q'):
     break
 # When everything done, release the capture
+print (frame.shape)
+print (len(face))
 cap.release()
 cv.destroyAllWindows()
 
