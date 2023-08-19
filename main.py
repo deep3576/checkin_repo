ea1013135f2be0fee599baba3672661f0f57675c
InderdeepSingh <deep3576@gmail.com>
2023-08-19 10:21:17 -0400

    update

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 cap = cv.VideoCapture(0)
 img_counter = 0
+a=0
+b=0
+Gw=0
+Gh=0
 face_classifier = cv.CascadeClassifier(
     cv.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
@@ -16,17 +20,23 @@ while True:
  if not ret:
     print("Can't receive frame (stream end?). Exiting ...")
  face = face_classifier.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
- if len(face)==1:
-    img_name = f'opencv_frame{img_counter}.png'
-    img_counter=img_counter+1
-        # saves the image as a png file
-    cv.imwrite(img_name,frame)
-    print('screenshot taken')
-    break
+
  for (x, y, w, h) in face:
     cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    frame=frame[x:x+w,y:y+h]
+    a=x
+    Gh=h
+    Gw=w
+    b=y
 
 
+ # if len(face)==1:
+ #    img_name = f'opencv_frame{img_counter}.png'
+ #    img_counter=img_counter+1
+ #        # saves the image as a png file
+ #    cv.imwrite(img_name,frame)
+ #    print('screenshot taken')
+ #    break
 
  #cv.rectangle(face)
 
@@ -37,6 +47,11 @@ while True:
 # When everything done, release the capture
 print (frame.shape)
 print (len(face))
+print ("Value of a is ")
+print (a)
+print ("Value of b is ")
+print (b)
+
 cap.release()
 cv.destroyAllWindows()
 
